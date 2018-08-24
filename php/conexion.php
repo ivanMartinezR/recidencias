d0679352a46c0ed835d725653fd3780fbbbeb389
ivanMartinezR <ivan_17_carro@hotmail.com>
2018-08-23 23:14:44 -0500

    archivo de conexion

---
+++ php/conexion.php
@@ -0,0 +1,26 @@
+<?php
+  function conectar(){
+     $host="localhost";
+     $usu="root";
+     $pwd="";	 
+     $db="dentista";
+	 if(!($link=mysqli_connect($host,$usu,$pwd))){
+	    echo "error al conectar con el host remoto";
+		exit();
+	 }
+	 if(!mysqli_select_db($link,$db)){
+	    echo "error al conectar con la base de datos".$db;
+		exit();
+	 }
+	 
+  }
+  conectar();
+
+
+
+
+
+
+  
+
+
