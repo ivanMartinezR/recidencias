b080cf230cd1762ad219bcdff15be2ba9f6e7358
unknown <IvanMartinezR>
2018-08-27 18:09:08 -0500

    casi está

--- js/controler/cita.js
+++ js/controler/cita.js
@@ -3,6 +3,8 @@
  $("#btnGuardarCita").click(function(){
    var minutos=document.getElementById("minutos").value;
    var hora=document.getElementById("hora").value;
+
+   
    var cita=$("#frmCitaNueva")[0];
 
 
@@ -15,15 +17,72 @@
         fechaCita:cita[3].value,
         horaCita:hora,
         minutosCita:minutos,
+        servicio:cita[6].value,
         
     },
     function(data, status){
-       console.log("Data: " + data + "\nStatus: " + status)
-       if(data==1)
-       	alert("Cita Registrada")
-       else
-       	alert("Ocurrió un Error")
+       console.log("Data: " + data + "\nStatus: " + status);
+       alert(data);
 
     });
 
 });
+
+
+ function Load(){
+    CargarComboServicios()
+    listarCitas()
+ }
+
+ function CargarComboServicios(){
+     $.post("php/cita.php",
+    {
+        accion: "GET_SERVICIOS",
+
+        
+    },
+    function(data, status){
+
+        $("#cmbServicio").html(data); 
+
+    });
+ }
+
+ function listarCitas(){
+    var fecha=new Date();
+    $.post("php/cita.php",
+    {
+        accion: "GET_CITAS",
+        fechaIni:fecha.getFullYear()+"-"+fecha.getMonth()+"-"+fecha.getDate(),
+        fechaFin:fecha.getFullYear()+"-"+fecha.getMonth()+"-"+fecha.getDate(),
+    },
+    function(data, status){
+
+        $("#listaCitas").html(data); 
+
+    });
+ }
+
+ $("#btnBuscarPorFecha").click(function(e){
+    var ini=$("#FechasEnModal :input")[0].value;
+    var fin=$("#FechasEnModal :input")[1].value; 
+    console.log(ini=="") 
+    if(ini!="" && fin!=""){
+        $.post("php/cita.php",
+        {
+            accion: "GET_CITAS",
+            fechaIni:ini,
+            fechaFin:fin,
+        },
+        function(data, status){
+            $("#btnCierraModalFechas").click();
+            $("#listaCitas").html(data); 
+
+        });
+    }else
+        alert("elije las fechas")
+     
+
+ });
+    
+
--- php/cita.php
+++ php/cita.php
@@ -13,16 +13,72 @@ if ($conn->connect_error) {
 
 if($_POST["accion"]=="GUARDA_CITA"){
 
-$fechaHora=date('Y-m-d H:i:s', strtotime($_POST["fechaCita"]." ".$_POST["horaCita"].":".$_POST["minutosCita"].":00"));
+	$fechaHora=date('Y-m-d H:i:s', strtotime($_POST["fechaCita"]." ".$_POST["horaCita"].":".$_POST["minutosCita"].":00"));
 
-$query="insert into cita(nom_paciente,telefono,email,fecha_cita) values('".$_POST["nombrePaciente"]."','".$_POST["telefonoPaciente"]."','".$_POST["email"]."','".$fechaHora."') ";
+	$consultaExiste="select concat(fecha_cita,' a ',date_add(fecha_cita,interval 1 hour)) as horarios from cita where '$fechaHora'>=fecha_cita and '$fechaHora'<=date_add(fecha_cita,interval 1 hour)";
+	$resultadoExiste=$conn->query($consultaExiste);
+
+   
+	if($resultadoExiste->num_rows>0){
+		$salida="Esta hora ya está ocupada! \n  ";
+		while ( $ren=$resultadoExiste->fetch_assoc()) {
+		     $salida.=" ".$ren["horarios"]." \n";
+	     }
+       echo $salida;
+
+	}else{
+
+		$query="insert into cita(nom_paciente,telefono,email,fecha_cita,id_servicio) values('".$_POST["nombrePaciente"]."','".$_POST["telefonoPaciente"]."','".$_POST["email"]."','".$fechaHora."',".$_POST["servicio"].") ";
 
 
 
-//echo $id=$conn->insert_id;
+		//echo $id=$conn->insert_id;
 
-echo $conn->query($query);
+		if($conn->query($query)){
+			echo "Consulta Capturada!";
+		}else{
+			echo "Ocurrió un error!";
+		}
+    }
  
 }
 
-  
+
+if($_POST["accion"]=="GET_SERVICIOS"){
+	$query="select * from servicio";
+	$salida="";
+	$result=$conn->query($query);
+
+	while ( $row=$result->fetch_assoc()) {
+		$salida.="<option value='".$row["id"]."'>".$row["nom_servicio"]."</option>";
+	}
+
+	echo $salida;
+}
+
+
+if($_POST["accion"]=="GET_CITAS"){
+
+    $query="select nom_paciente,fecha_cita,s.nom_servicio from cita as c join servicio as s on c.id_servicio=s.id where date(fecha_cita)>='".$_POST["fechaIni"]."' and date(fecha_cita)<='".$_POST["fechaFin"]."'";
+	$salida="<div id='cabeceroCitas' style='background-color:#00e4ff;' class='row'>
+					  <div class='col'>Paciente</div>
+					  <div class='col'>Fecha</div>
+					  <div class='col'>Hora</div>
+					
+                  </div>";
+
+
+	$result=$conn->query($query);
+
+	while ($row=$result->fetch_assoc()) {
+		$salida.="<div class='row'>
+					  <div class='col'>".$row["nom_paciente"]."</div>
+					  <div class='col'>".explode(" ", $row["fecha_cita"])[0]."</div>
+					  <div class='col'>".explode(" ", $row["fecha_cita"])[1]."</div>
+					
+                  </div>";
+	}
+
+	echo $salida;
+}
+
